2f39dd5336e793b6130b5a465cdebc16eca4a735
Kevin Ye <chunye@live.com>
2017-03-09 20:59:09 +0800

    Add project files.

---
+++ WebApplication3/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Filters;
+
+namespace WebApplication3.Controllers
+{
+    //[Authorize]
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+       [Log]
+        public ActionResult Search(string name="Chinese")
+        {
+            throw new Exception("Something terrible happened!");
+            var message = Server.HtmlEncode(name);
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About"});
+            //return File(Server.MapPath("~/Content/site.css"),"text/css");
+            //return Json(new { Message = message, Name = "Kevin" }, JsonRequestBehavior.AllowGet);
+            return Content(message);
+        }
+    }
+}
